9a5870b07f65d05c275d846d5dc376714c1e36fd
sufianfuad <sufianfuad2611@gmail.com>
2022-03-03 01:24:10 +0600

    stripe commentOut

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const express = require('express')
 const cors = require('cors')
 //mongodb
 const { MongoClient } = require('mongodb');
-
+const ObjectId = require('mongodb').ObjectId;
 const admin = require("firebase-admin");
-
+const fileUpload = require('express-fileupload');
 require('dotenv').config();
 
+//stripe
+// const stripe = require('stripe')(process.env.STRIPE_SECRET)
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -24,6 +26,7 @@ admin.initializeApp({
 //middleWare
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i3fcr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -50,15 +53,23 @@ async function run() {
         const database = client.db('doctors_portal')
         const appointmentsCollection = database.collection('appointments');
         const usersCollection = database.collection('users');
+        const doctorsCollection = database.collection('doctors');
 
         app.get('/appointments', verifyToken, async (req, res) => {
             const email = req.query.email;
-            const date = new Date(req.query.date).toLocaleDateString();
+            // const date = new Date(req.query.date).toLocaleDateString();
             // console.log(date)
-            const query = { email: email, date: date }
+            console.log(query);
+            const query = { email: email }
             const cursor = appointmentsCollection.find(query);
             const appointments = await cursor.toArray();
             res.json(appointments);
+        });
+        app.get('/appointments/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await appointmentsCollection.findOne(query);
+            res.json(result);
         })
 
         app.post('/appointments', async (req, res) => {
@@ -67,6 +78,22 @@ async function run() {
             console.log(result)
             res.json(result)
             // res.json({ message: 'Hello' })
+        });
+        //update
+        app.put('/appointments/:id', async (req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            const filter = { _id: ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    payment: payment
+                }
+            }
+            const result = await appointmentsCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        });
+        app.post('/doctors', async (req, res) => {
+            console.log()
         })
         //for special user like admin
         app.get('/users/:email', async (req, res) => {
@@ -117,8 +144,17 @@ async function run() {
             else {
                 res.status(403).json({ message: 'you do not have access to make admin' });
             }
-
-
+        })
+        app.post('/create-payment-intent', async (req, res) => {
+            const paymentInfo = req.body;
+            // aitar mane hocche poysha hishab kore
+            const amount = paymentInfo.price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                payment_method_types: ['card']
+            });
+            res.json({ clientSecret: paymentIntent.client_secret });
         })
     }
     finally {
